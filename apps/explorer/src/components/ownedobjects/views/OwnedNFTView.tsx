ac639670acafa97c4e572bc0f3852b86fe2cab76
Michael Amos <michael@mystenlabs.com>
2023-01-19 08:58:09 -0800

    grid

--- apps/explorer/src/components/ownedobjects/views/OwnedNFTView.tsx
+++ apps/explorer/src/components/ownedobjects/views/OwnedNFTView.tsx
@@ -8,18 +8,16 @@ import { ObjectDetails } from '~/ui/ObjectDetails';
 
 export default function OwnedNFTView({ results }: { results: DataType }) {
     return (
-        <div className="flex w-full justify-start">
-            <ul className="grid grid-cols-1 gap-6 pl-0 sm:grid-cols-2 lg:grid-cols-2">
-                {results.map((entryObj, idx) => (
-                    <ObjectDetails
-                        id={entryObj.id}
-                        key={`${entryObj.name}-${idx}`}
-                        name={entryObj.name}
-                        image={entryObj.display}
-                        type={trimStdLibPrefix(entryObj.Type)}
-                    />
-                ))}
-            </ul>
-        </div>
+        <ul className="grid grid-cols-1 content-start justify-start gap-6 pl-0 sm:grid-cols-2 lg:grid-cols-2">
+            {results.map((entryObj, idx) => (
+                <ObjectDetails
+                    id={entryObj.id}
+                    key={`${entryObj.name}-${idx}`}
+                    name={entryObj.name}
+                    image={entryObj.display}
+                    type={trimStdLibPrefix(entryObj.Type)}
+                />
+            ))}
+        </ul>
     );
 }
