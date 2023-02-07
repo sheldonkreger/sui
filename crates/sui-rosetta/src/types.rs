2354698e6cd8ff8854fbdc75ac18e96a64c1beba
patrick <patrickkuo@me.com>
2023-02-07 11:19:05 -0800

    include gas price when doing coin selection

--- crates/sui-rosetta/src/types.rs
+++ crates/sui-rosetta/src/types.rs
@@ -579,6 +579,7 @@ pub struct ConstructionMetadata {
     pub tx_metadata: TransactionMetadata,
     pub sender: SuiAddress,
     pub gas: ObjectRef,
+    pub gas_price: u64,
     pub budget: u64,
 }
 
@@ -914,11 +915,12 @@ impl InternalOperation {
             }
         };
 
-        Ok(TransactionData::new_with_dummy_gas_price(
+        Ok(TransactionData::new(
             TransactionKind::Single(single_tx),
             metadata.sender,
             metadata.gas,
             metadata.budget,
+            metadata.gas_price,
         ))
     }
 }
